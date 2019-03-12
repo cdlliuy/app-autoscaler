bff7dcd10c0b22a5f004852ac8e21fd8bb1c9f50
Liu Ying <cdlliuy@cn.ibm.com>
2019-03-12 14:44:30 +0800

    fix

--- src/autoscaler/metricscollector/collector/app_streamer.go
+++ src/autoscaler/metricscollector/collector/app_streamer.go
@@ -56,7 +56,7 @@ func (as *appStreamer) Stop() {
 }
 
 func (as *appStreamer) streamMetrics() {
-	as.noaaConsumer.SetIdleTimeout（5 * time.Minute)
+	as.noaaConsumer.SetIdleTimeout(5 * time.Minute)
 	eventChan, errorChan := as.noaaConsumer.Stream(as.appId, cf.TokenTypeBearer+" "+as.cfc.GetTokens().AccessToken)
 	ticker := as.sclock.NewTicker(as.collectInterval)
 	defer ticker.Stop()
